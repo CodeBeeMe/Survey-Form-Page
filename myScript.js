d502f82f875a956d64eb96f7926733396a95881b
Cătălin Anghel-Ursu <catalee.xen@gmail.com>
2018-07-04 18:19:19 +0300

    Create myScript.js
    Added the .js file for the completed project.

---
+++ myScript.js
@@ -0,0 +1,29 @@
+/* !Date:03.07.2018  Copyright ©2018 JavaScript code by Cătălin Anghel-Ursu @Madness2aMaze (https://codepen.io/Madness2aMaze)
+- All Rights Reserved!
+
+MIT License
+
+Copyright (c) 2018 Cătălin Anghel-Ursu (https://github.com/Madness2aMaze/Survey-Form-Page)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+function justOne(check) {
+  const checkboxes = document.getElementsByName('check') //get an array of all the elements with the name attribute "check"
+  checkboxes.forEach((el) => el !== check ? el.checked = false : null);
+}
